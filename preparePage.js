28c18e5ee13214b117f8afa82c2ab459ca1722c0
Marco Endres <marco.endres@clavisit.com>
2019-04-18 16:24:25 +0200

    Copy the Vue call and save it

--- preparePage.js
+++ preparePage.js
@@ -7,8 +7,19 @@ module.exports = function(options, body) {
     var vueComponents = [];
 
     $('*[data-portlet-type="vue"]').each(function() {
-        vueComponents.push($(this).attr('id'))
-        $(this).next('script').remove()
+        var scriptContent = $(this).next('script').html()
+        var initText = 'new Vue('
+        var vueContentStart = scriptContent.indexOf(initText);
+        var vuePart;
+        if(vueContentStart > 0) {
+            var vueContentEnd = findClosingBracketMatchIndex(scriptContent,vueContentStart + initText.length - 1)
+            if (vueContentEnd > 0) {
+                vuePart = scriptContent.substring(vueContentStart, vueContentEnd + 1)
+            }
+        }
+
+        var component = new VueComponent($(this).attr('id'), vuePart)
+        vueComponents.push(component)
     });
 
     var text = $.html();
@@ -18,3 +29,27 @@ module.exports = function(options, body) {
     fs.writeFileSync(filename, text, 'utf8');
 }
 
+function findClosingBracketMatchIndex(str, pos) {
+    if (str[pos] != '(') {
+        throw new Error("No '(' at index " + pos);
+    }
+    let depth = 1;
+    for (let i = pos + 1; i < str.length; i++) {
+        switch (str[i]) {
+            case '(':
+                depth++;
+                break;
+            case ')':
+                if (--depth == 0) {
+                    return i;
+                }
+                break;
+        }
+    }
+    return -1;    // No matching closing parenthesis
+}
+
+function VueComponent (id, content) {
+    this.id = id;
+    this.content = content;
+}
